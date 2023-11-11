729285b469bbd6ece089890e5b3461a5833a1962
raghu309 <ragavendrarag309@gmail.com>
2023-11-11 16:18:30 +0530

    first commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,21 @@
+const PlaceHolderURL = "https://via.placeholder.com/400";
+
+const MovieCard = ({movie}) => {
+    return (
+    <div className="movie">
+        <div>
+            <p>{movie.Year}</p>
+        </div>
+        <div>
+            <img src={movie.Poster!=="N/A"?movie.Poster:PlaceHolderURL} alt={movie.Title} />
+        </div>
+
+        <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+        </div>
+    </div>
+    );
+}
+
+export default MovieCard;
